48ffd4a4de51b095b84e5f7f48e11c8bf7e81b23
ofirfrancis <ofir.francis@gmail.com>
2021-03-15 13:41:16 +0000

    Modify file structure to Flask standard

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Scenario-2
+## Goal : Implement a web-application for automata exercises
+### Made with Python3/Flask/HTML/CSS
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from flask.templating import render_template
+from werkzeug.utils import redirect
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+        return render_template("index.html")
+
+@app.route("/who_are_we")
+def whoAreWe():
+    return render_template("who_are_we.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
+    app.run()
