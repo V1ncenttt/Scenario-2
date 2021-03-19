97301da37bed7964c2882937e9e421b4509db458
ofirfrancis <ofir.francis@gmail.com>
2021-03-19 16:29:22 +0000

    Add connection to database for exercises

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, request
-from flask.helpers import flash
 from flask.templating import render_template
 from operations import *
+import login_data
+import pymysql
+
+conn = pymysql.connect(host=login_data.HOST,
+                        port=login_data.PORT,
+                        user=login_data.USER,
+                        password=login_data.PASSWORD,
+                        database=login_data.DATABASE)
+c = conn.cursor()
 
 
 app = Flask(__name__)
@@ -20,7 +28,7 @@ def contact():
 
 @app.route("/exercises")
 def exercies():
-    return render_template("exercises.html", data = [{"id":"1", "type": "test", "title":"exercise", "description":"Try to find a regular expression for this NFA", "difficulty":"40", "date":"2021-03-16"}])
+    return render_template("exercises.html", data = getAll())
 
 @app.route("/about")
 def about():
@@ -47,10 +55,11 @@ def generator():
     return render_template("generator.html")
 
 
-@app.route("/exercise-<exNum>", methods = ['POST', 'GET'])
+@app.route("/exercise-<int:exNum>", methods = ['POST', 'GET'])
 def exercise(exNum):
-    if request.method == "GET":
-        return render_template("exercise_template.html",data = {"id":"1", "type": "test", "title":"exercise", "description":"Try to find a regular expression for this NFA", "difficulty":"40", "date":"2021-03-16"})
+    if request.method == "GET":    
+        print(type(exNum))
+        return render_template("exercise_template.html",data = getEx(exNum))
     if request.method == "POST":
         print(request.form)
         return render_template("sample_exercise_page.html")
@@ -69,6 +78,33 @@ def add_header(response):
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Expires'] = '0'
     return response
+
+
+
+
+
+
+def getAll():     
+    c.execute("SELECT * from Exercises")
+    rows = c.fetchall()
+    data = []
+    for row in rows:
+        data.append({"id":row[0], "type": row[1], "title":row[2], "description":row[3], "difficulty":row[4], "date":row[5].strftime("%Y-%m-%e")})
+    return data
+
+
+def getEx(exNum):
+    c.execute("SELECT * from Exercises WHERE id=" + str(exNum))
+    rows = c.fetchall()
+    row = rows[0]
+    data = {"id":row[0], "type": row[1], "title":row[2], "description":row[3], "difficulty":row[4], "date":row[5].strftime("%Y-%m-%e")}
+    return data
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
